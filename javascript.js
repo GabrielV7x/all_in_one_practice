68131020a92450b2c94562270db322fa1f5e964e
Frederic Feliciane <fredericfeliciane8@gmail.com>
2022-11-24 15:42:42 +0400

    html, css and js files created

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+const electric = document.querySelector('.electric');
+console.log(electric)
+
+
+electric.addEventListener("click", changeColor());
+
+function changeColor() {
+  electric.classList.add('yellow');
+  console.log("changeColor function has been called")
+}
