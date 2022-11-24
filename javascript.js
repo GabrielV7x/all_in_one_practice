f282fa839415762ebd70c307263d688960bc7814
Frederic Feliciane <fredericfeliciane8@gmail.com>
2022-11-24 23:54:04 +0400

    using data attribute as variable done

--- javascript.js
+++ javascript.js
@@ -3,11 +3,9 @@
 
 // select pokemon by type
 const electric = document.querySelectorAll('.electric');
-// console.log(electric)
 
 //create a button to listen to the event
 const buttonElectric = document.querySelector('.btn_electric')
-// console.log(buttonElectric)
 
 // add event event on button to listen
 buttonElectric.addEventListener("click", changeColor)
@@ -15,8 +13,9 @@ buttonElectric.addEventListener("click", changeColor)
 // function called, forEach loop used to acces each pokemon to apply class changes.
 function changeColor() {
   electric.forEach(pokemon => {
-    pokemon.classList.toggle('yellow');
+    const color = pokemon.getAttribute("data-color")
+    // console.log(color)
+    pokemon.classList.toggle(color);
     console.log("changeColor function has been called")
   });
-
 }
