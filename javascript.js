4d6283c97d2515ee459d58729814887a77592931
Frederic Feliciane <fredericfeliciane8@gmail.com>
2022-11-24 23:14:21 +0400

    electric button added

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 
+// when click on a pokemon type, background color of the matching pokemon type change.
 
-const electric = document.querySelector('.electric');
-console.log(electric)
+// select pokemon by type
+const electric = document.querySelectorAll('.electric');
+// console.log(electric)
 
+//create a button to listen to the event
+const buttonElectric = document.querySelector('.btn_electric')
+// console.log(buttonElectric)
 
-electric.addEventListener("click", changeColor);
+// add event event on button to listen
+buttonElectric.addEventListener("click", changeColor)
 
+// function called, forEach loop used to acces each pokemon to apply class changes.
 function changeColor() {
-  electric.classList.add('yellow');
-  console.log("changeColor function has been called")
+  electric.forEach(pokemon => {
+    pokemon.classList.toggle('yellow');
+    console.log("changeColor function has been called")
+  });
+
 }
